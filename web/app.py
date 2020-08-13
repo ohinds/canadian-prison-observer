cf37c884e501ff63c2541db3831265cda534dcea
ohinds <ohinds@gmail.com>
2020-08-13 10:19:34 -0400

    Switch away from dash to Observable.
    Now this python
    - Generates the pie JSON
    - Builds a docker container to serve a simple website that points to observable

--- web/app.py
+++ web/app.py
@@ -1,108 +1,30 @@
-"""App to reveal the scale and shape of the Canadian carceral system.
+from flask import Flask
+app = Flask(__name__)
+
+html = """
+<html>
+<head>
+<title>
+Canada Prison Initiative - The Whole Pie, Canada
+</title>
+</head>
+<body>
+<div id="observablehq-80b000b4"></div>
+<script type="module">
+import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
+import define from "https://api.observablehq.com/@observablehq/downloading-and-embedding-notebooks.js?v=3";
+const inspect = Inspector.into("#observablehq-80b000b4");
+(new Runtime).module(define, name => name === "graphic" ? inspect() : undefined);
+</script>
+</body>
+</html>
 """
 
-import dash
-from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objs as go
 
-from visualize.pie import Pie
-
-
-external_stylesheets = [
-    'https://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic&subset=latin,latin-ext',
-]
-
-app = dash.Dash(
-    __name__,
-    external_stylesheets=external_stylesheets,
-    meta_tags=[
-        {'name': 'viewport',
-         'content': 'width=device-width, initial-scale=1.0',
-        }
-    ],
-)
-app.title = "The Canadian Carceral System"
-server = app.server
-
-pies = {
-    'population': Pie('population'),
-    'admissions': Pie('admissions')
-}
-for pie in pies.values():
-    pie.build()
-year_slider_marks = {i: {'label': f'{i}/{i+1}'} for i in range(2000, 2018)}
-
-empty_layout = {
-    'paper_bgcolor': 'rgba(0,0,0,0)',
-    'plot_bgcolor': 'rgba(0,0,0,0)',
-    'xaxis': dict(
-        showline=False, showgrid=False, zeroline=False, showticklabels=False
-    ),
-    'yaxis': dict(
-        showgrid=False, showline=False, zeroline=False, showticklabels=False
-    ),
-}
-
-
-app.layout = html.Div(
-    id='root',
-    children=[
-        html.Div(
-            html.Div("The Canadian Carceral System",
-                     className='h1'),
-            id='title',
-        ),
-        html.Div(
-            id='app-container',
-            children=[
-                dcc.Dropdown(
-                    id='pie-dropdown',
-                    options=[{'label': pie_name.title(), 'value': pie_name}
-                             for pie_name in pies]
-                ),
-                html.Div(
-                    id='pie-container',
-                    className='row flex-display',
-                    children=[
-                        html.Div(
-                            dcc.Slider(
-                                id='year-slider',
-                                min=min(year_slider_marks),
-                                max=max(year_slider_marks),
-                                value=max(year_slider_marks),
-                                marks=year_slider_marks,
-                                vertical=True,
-                            ),
-                            className='one column',
-                        ),
-                        html.Div(
-                            dcc.Graph(
-                                id='pie',
-                                figure=go.Figure(layout=empty_layout),
-                                config={'displayModeBar': False},
-                            ),
-                            className='eleven columns',
-                        )
-                    ]
-                ),
-            ]
-        ),
-    ]
-)
-
-
-@app.callback(Output('pie', 'figure'), [
-    Input('pie-dropdown', 'value'),
-    Input('year-slider', 'value'),
-])
-def choose_pie(pie_name, year):
-    if not pie_name:
-        return go.Figure(layout=empty_layout)
-    pies[pie_name].build(year=year_slider_marks[year]['label'])
-    return pies[pie_name].get_figure()
+@app.route("/")
+def serve():
+    return html
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
