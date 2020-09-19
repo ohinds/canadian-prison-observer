8370a1e406cc35b3467155ff505e0969fd8f7787
ohinds <ohinds@gmail.com>
2020-09-19 16:18:36 -0400

    Add gender graph.

--- web/app.py
+++ web/app.py
@@ -1,48 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-html = """
-<html>
-<head>
-<title>
-Canadian Prison Observer
-</title>
-</head>
-<body>
-<div id="observablehq-829185ef">
-  <div class="observablehq-inputs"></div>
-  <div class="observablehq-viewof-sunburst"></div>
-  <div class="observablehq-type"></div>
-</div>
-<script type="module">
-  import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-  import define from "https://api.observablehq.com/@ohinds/the-whole-pie-canada.js?v=3";
-  (new Runtime).module(define, name => {
-    if (name === "inputs") return Inspector.into("#observablehq-829185ef .observablehq-inputs")();
-    if (name === "viewof sunburst") return Inspector.into("#observablehq-829185ef .observablehq-viewof-sunburst")();
-    if (name === "type") return Inspector.into("#observablehq-829185ef .observablehq-type")();
-  });
-</script>
-<div id="observablehq-252f8b55">
-  <div class="observablehq-inputs"></div>
-  <div class="observablehq-chart"></div>
-</div>
-<script type="module">
-  import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-  import define from "https://api.observablehq.com/@ohinds/graphs-by-province.js?v=3";
-  (new Runtime).module(define, name => {
-    if (name === "inputs") return Inspector.into("#observablehq-252f8b55 .observablehq-inputs")();
-    if (name === "chart") return Inspector.into("#observablehq-252f8b55 .observablehq-chart")();
-  });
-</script>
-</body>
-</html>
-"""
-
 
 @app.route("/")
 def serve():
-    return html
+    return open('index.html').read()
 
 
 if __name__ == '__main__':
