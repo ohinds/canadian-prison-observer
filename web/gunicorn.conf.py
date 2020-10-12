96b0b3b2c06b924540cdd686e78b7387791e9795
ohinds <ohinds@gmail.com>
2020-10-12 12:20:41 -0400

    Lightweight web server for reliability

--- web/gunicorn.conf.py
+++
@@ -1,7 +0,0 @@
-import multiprocessing
-workers = multiprocessing.cpu_count() * 2 + 1
-worker_connections = 10
-timeout = 100
-loglevel = 'warning'
-accesslog = 'gunicorn-access.log'
-keepalive = 650
