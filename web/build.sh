56b53682bb891b337ecd32d6a33d20e397fbc9e7
ohinds <ohinds@gmail.com>
2020-10-14 14:38:47 -0400

    move to app engine

--- web/build.sh
+++
@@ -1,2 +0,0 @@
-docker build -t gcr.io/`gcloud config get-value project`/canada-prison-initiative:latest -f Dockerfile . && \
-    docker push gcr.io/`gcloud config get-value project`/canada-prison-initiative:latest
